376fcbbdef564ecafe33706b8f109b5a37dc19c7
Messi002 <rostandmessi2@gmail.com>
2023-09-05 20:14:19 +0100

    added some git commands

--- object.js
+++ object.js
@@ -31,3 +31,11 @@ console.log(`${user3 == user4}`);
 // user3   
 //   ====> {name: "John", age: 30}
 //user4
+
+
+/*
+
+HELLO SIR, I JUST CREATED A NEW BRANCH IN THIS REPOSITORY
+git checkout -b <BRANCH-NAME> 
+
+*/
