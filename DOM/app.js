872216850b8a0ed07c6c514a5eb66991130901a6
Messi002 <rostandmessi2@gmail.com>
2023-09-28 19:00:35 +0100

    worked on document object model

---
+++ DOM/app.js
@@ -0,0 +1,45 @@
+const p = document.querySelector('p');
+const ol = document.getElementById('ol');   
+const div2 = document.getElementById('div');   
+
+
+const div = document.createElement('div');
+div.className = 'alert'
+div.innerHTML = '<strong>Hi there!</strong> You\'ve this message'
+p.append(div)
+
+//before
+// prependfield
+// append
+// after
+ol.before('hello')
+
+let liFirst = document.createElement('li'); 
+liFirst.innerHTML = 'prepend is here';
+ol.prepend(liFirst)
+
+
+let liLast = document.createElement('li'); 
+liLast.innerHTML = 'append is here';
+ol.append(liLast)
+
+//after
+ol.after('hi it is after')
+
+
+//insertAdjacentHTML
+// beforebegin -> insert html immediately before elem, 
+//beforeend -> insert html into elem, at the end, 
+//afterend -> insert html immediately after elem, 
+// afterbegin -> insert html into the elm at the beginning
+div2.insertAdjacentHTML('afterbegin', '<strong>hello</strong>')
+
+
+p.style.cssText = `
+background-color: yellow;
+text-align: center;
+color: red;
+`;
+
+
+div2.classList.add('alert');
--- promise.js
+++ promise.js
@@ -46,3 +46,5 @@ fetchTodos().then((todos) => {
     })
 })
 
+
+
